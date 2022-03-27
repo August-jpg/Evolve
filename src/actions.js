4de5ebbe83a0c8ebbf119a3227550a75edd4de0c
August Kuehl <turtlethedragon@icloud.com>
2022-03-27 12:01:36 -0500

    Update actions.js

--- src/actions.js
+++ src/actions.js
@@ -70,8 +70,8 @@ export const actions = {
             title: loc('evo_organelles_title'),
             desc: loc('evo_organelles_desc'),
             cost: {
-                RNA(){ return 1; },
-                DNA(){ return 1; }
+                RNA(offset){ return evolveCosts('organelles',12,8,offset); },
+                DNA(offset){ return evolveCosts('organelles',4,4,offset); }
             },
             effect(){
                 let rna = global.race['rapid_mutation'] ? 2 : 1;
